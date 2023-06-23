1fa520714f14e661c6d159ddf53020a79fd0615d
danielbatres <danielbatresuser@gmail.com>
2023-06-23 16:00:13 -0600

    Creation of a dependency for injection

---
+++ Services/HelloWorldService.cs
@@ -0,0 +1,9 @@
+public class HelloWorldService : IHelloWorldService {
+  public string GetHelloWorld() {
+    return "Hello World!";
+  }
+}
+
+public interface IHelloWorldService {
+  string GetHelloWorld();
+}
