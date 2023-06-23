606897363e665420a00ca51f7dff11a28420c01e
danielbatres <danielbatresuser@gmail.com>
2023-06-23 16:08:32 -0600

    Another way to add scoped

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IHelloWorldService, HelloWorldService>();
+//builder.Services.AddScoped<IHelloWorldService, HelloWorldService>();
+builder.Services.AddScoped<IHelloWorldService>(p => new HelloWorldService());
 
 var app = builder.Build();
 
