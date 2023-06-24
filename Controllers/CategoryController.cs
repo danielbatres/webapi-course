721b782e53536218f5c6410ae76305149cbfb64f
danielbatres <danielbatresuser@gmail.com>
2023-06-24 13:26:06 -0600

    Category controller

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using webapi_course.Models;
+using webapi_course.Services;
+
+namespace webapi_course.Controllers;
+
+[Route("api/[controller]")]
+public class CategoryController : ControllerBase {
+  ICategoryService categoryService;
+
+  public CategoryController(ICategoryService service) {
+    categoryService = service;
+  }
+
+  [HttpGet]
+  public IActionResult Get() {
+    return Ok(categoryService.Get());
+  }
+
+  [HttpPost]
+  public IActionResult Post([FromBody] Category category) {
+    categoryService.Save(category);
+
+    return Ok();
+  }
+
+  [HttpPut("{id}")]
+  public IActionResult Put(Guid id, [FromBody] Category category) {
+    categoryService.Update(id, category);
+
+    return Ok();
+  }
+
+  [HttpDelete("{id}")]
+  public IActionResult Delete(Guid id) {
+    categoryService.Delete(id);
+
+    return Ok();
+  }
+}
