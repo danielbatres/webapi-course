6536b3ff5af1318921b0e395ee9f5dc91b6660c7
danielbatres <danielbatresuser@gmail.com>
2023-06-24 13:07:39 -0600

    Category service

---
+++ Services/CategoryService.cs
@@ -0,0 +1,48 @@
+using webapi_course.Models;
+
+namespace webapi_course.Services;
+
+public class CategoriesService : ICategoryService {
+  AssignmentsContext context;
+
+  public CategoriesService(AssignmentsContext dbContext) {
+    context = dbContext;
+  }
+
+  public IEnumerable<Category> Get() {
+    return context.Categories;
+  }
+
+  public async Task Save(Category category) { 
+    context.Add(category);
+    await context.SaveChangesAsync();
+  }
+
+  public async Task Update(Guid id, Category category) {
+    var ActualCategory = context.Categories.Find(id);
+
+    if (ActualCategory != null) {
+      ActualCategory.Name = category.Name;
+      ActualCategory.Description = category.Description;
+      ActualCategory.Weight = category.Weight;
+
+      await context.SaveChangesAsync();
+    }
+  }
+
+  public async Task Delete(Guid id) {
+    var ActualCategory = context.Categories.Find(id);
+
+    if (ActualCategory != null) {
+      context.Remove(ActualCategory);
+      await context.SaveChangesAsync();
+    }
+  }
+}
+
+public interface ICategoryService {
+  IEnumerable<Category> Get();
+  Task Save(Category category);
+  Task Update(Guid id, Category category);
+  Task Delete(Guid id);
+}
