5ff125e3c9d619feb5ef1418d9593cb8df5bbd86
danielbatres <danielbatresuser@gmail.com>
2023-06-24 14:12:38 -0600

    Connection confirmation

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -6,13 +6,23 @@ namespace webapi_course.Controllers;
 [Route("api/[controller]")]
 public class HelloWorldController : ControllerBase {
   IHelloWorldService helloWorldService;
+  protected readonly AssignmentsContext context;
 
-  public HelloWorldController(IHelloWorldService helloWorld) { 
+  public HelloWorldController(IHelloWorldService helloWorld, AssignmentsContext dbContext) { 
     helloWorldService = helloWorld;
+    context = dbContext;
   }
 
   [HttpGet]
   public IActionResult Get() {
     return Ok(helloWorldService.GetHelloWorld());
+  }
+
+  [HttpGet]
+  [Route("createdb")]
+  public IActionResult CreateDatabase() {
+    context.Database.EnsureCreated();
+
+    return Ok();
   }
 }
